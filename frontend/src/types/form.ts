705b2734dfa3e1e9c7b5367f408e9ecc40513bad
AlonsoMCDG <alonsomartins056@gmail.com>
2026-04-10 11:06:02 -0500

    e adicionar tipo 'password'

--- frontend/src/types/form.ts
+++ frontend/src/types/form.ts
@@ -15,7 +15,7 @@ export interface TableSchema {
 
 export type FieldType = 
   | 'text' | 'number' | 'date' | 'datetime-local' | 'checkbox' | 'textarea' 
-  | 'select' | 'datalist' | 'hidden' | 'email' | 'tel';
+  | 'select' | 'datalist' | 'hidden' | 'email' | 'tel' | 'password';
 
 // Interface para os botões de ação
 export interface QuickAction {
