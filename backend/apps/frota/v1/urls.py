c83e04ea52d52d4a586a67bdfcadd210a9859729
AlonsoMCDG <alonsomartins056@gmail.com>
2026-04-29 15:06:24 -0300

    fix: arrumar imports em urls.py

--- backend/apps/frota/v1/urls.py
+++ backend/apps/frota/v1/urls.py
@@ -1,11 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import VeiculoViewSet, RotaViewSet, TipoCombustivelViewSet, TipoVeiculoViewSet
+from .views import VeiculoViewSet, RotaViewSet, TipoCombustivelViewSet
 
 router = DefaultRouter()
 
 router.register(r'veiculos', VeiculoViewSet, basename='veiculo')
 router.register(r'rotas', RotaViewSet, basename='rota')
 router.register(r'tipos-combustivel', TipoCombustivelViewSet, basename='tipo-combustivel')
-router.register(r'tipos-veiculo', TipoVeiculoViewSet, basename='tipo-veiculo')
 
 urlpatterns = router.urls
--- backend/apps/operacao/v1/urls.py
+++ backend/apps/operacao/v1/urls.py
@@ -1,10 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import OperadorVeiculoViewSet, GuiaViewSet, TipoServicoViewSet, AlocacaoServicoViewSet
+from .views import OperadorVeiculoViewSet, GuiaAbastecimentoViewSet, TipoAtividadeViewSet, AlocacaoServicoViewSet
 
 router = DefaultRouter()
 
 # Rota final: /api/v1/tipos-servico/
-router.register(r'tipos-servico', TipoServicoViewSet, basename='tipo-servico')
+router.register(r'tipos-servico', TipoAtividadeViewSet, basename='tipo-servico')
 
 # Rota final: /api/v1/alocacoes-servicos/
 router.register(r'alocacoes-servicos', AlocacaoServicoViewSet, basename='alocacao-servico')
@@ -13,6 +13,6 @@ router.register(r'alocacoes-servicos', AlocacaoServicoViewSet, basename='alocaca
 router.register(r'operadores-veiculos', OperadorVeiculoViewSet, basename='operador-veiculo')
 
 # Rota final: /api/v1/guias/
-router.register(r'guias', GuiaViewSet, basename='guia')
+router.register(r'guias', GuiaAbastecimentoViewSet, basename='guia')
 
 urlpatterns = router.urls
