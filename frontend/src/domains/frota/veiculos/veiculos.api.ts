3d1cc65eccc9a425e477b010127e89b1d9f9d7ae
AlonsoMCDG <alonsomartins056@gmail.com>
2026-08-11 09:22:23 -0500

    refactor(backend|Veiculo): corrigir a api de veiculos

--- frontend/src/domains/frota/veiculos/veiculos.api.ts
+++ frontend/src/domains/frota/veiculos/veiculos.api.ts
@@ -1,9 +1,9 @@
 import { createCrudApi } from "../../../core/api/crudFactory";
 import { ENDPOINTS } from "../../../core/api/endpoints";
 
-import { veiculoReadSchema } from "./schemas/veiculo.read.zod";
-import { veiculoWriteSchema } from "./schemas/veiculo.write.zod";
-import type { VeiculoListParams } from "./schemas/veiculo.filters.zod";
+import { veiculoReadSchema } from "./schemas-zod/veiculo.read.zod";
+import { veiculoWriteSchema } from "./schemas-zod/veiculo.write.zod";
+import type { VeiculoListParams } from "./schemas-zod/veiculo.filters.zod";
 
 export const veiculosApi = createCrudApi<
   typeof veiculoReadSchema,
